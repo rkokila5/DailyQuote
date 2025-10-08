8cc6facf219715e032eefe9ddc450ccfef581480
Kokila <rkokilamac@gmail.com>
2025-10-08 19:44:43 +0530

    Initial Commit

---
+++ DailyQuote/DailyQuoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyQuoteApp.swift
+//  DailyQuote
+//
+//  Created by Kokila on 08/10/25.
+//
+
+import SwiftUI
+
+@main
+struct DailyQuoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
